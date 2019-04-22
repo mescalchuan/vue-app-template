ea223614533ca67a041ae41327472f09df4cbbac
Mescal Chuan <1335382915@qq.com>
2019-04-22 12:30:58 +0800

    Update home.js

--- entry/home.js
+++ entry/home.js
@@ -1,6 +1,10 @@
-import home from "@/components/home/index";
+import home from "@/components/home/index"
 //使用阿里flexible解决方案
-import 'amfe-flexible';
+import 'amfe-flexible'
+//VConsole
+import VConsole from 'vconsole'
+
+const vconsole = new VConsole()
 
 new Vue({
   el: '#app',
