2c84e08d0df931266e786ecc715fe5fc2739cf39
qinchuan <qinchuan@qinchuandeMacBook-Pro.local>
2019-04-22 14:35:46 +0800

    add vconsole-webpack-plugin

--- entry/home.js
+++ entry/home.js
@@ -1,10 +1,6 @@
 import home from "@/components/home/index"
 //使用阿里flexible解决方案
 import 'amfe-flexible'
-//VConsole
-import VConsole from 'vconsole'
-
-const vconsole = new VConsole()
 
 new Vue({
   el: '#app',
